755bf9f0d639983672fc09cf0d833c925dd93a94
Stylianos <Stylianos.Antonopoulos-CIC.Netherlands>
2020-10-30 10:16:23 +0100

    fix import in MapElementIterface.ts

--- src/interfaces/MapElementInterface.ts
+++ src/interfaces/MapElementInterface.ts
@@ -1,7 +1,7 @@
 import { MapAttributesInterface } from './MapAttributesInterface';
 import { JsonToXmlMapInterface } from './JsonToXmlMapInterface';
 import { MapElementTypeEnum } from '../enums/MapElementEnum';
-import { NodeTypeEnum } from '../../../cbp/nl-afrr-activated-energy-forwarding/server/src/core/activated-energy/enums/NodeTypeEnum';
+import { NodeTypeEnum } from '../enums/NodeTypeEnum';
 
 export interface MapElementInterface {
   xmlNodeName: string;
